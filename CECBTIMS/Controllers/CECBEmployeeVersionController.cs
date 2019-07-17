030b3a6c6330727a3110a05984823e6f86019ac3
chamodya <chamodyawimansha@gmail.com>
2019-07-17 12:42:27 +0530

    with fixed errors. everything works

--- CECBTIMS/Controllers/CECBEmployeeVersionController.cs
+++ CECBTIMS/Controllers/CECBEmployeeVersionController.cs
@@ -11,11 +11,11 @@ namespace CECBTIMS.Controllers
     public class CECBEmployeeVersionController : Controller
     {
         private CECB_ERPEntities db = new CECB_ERPEntities();
-
-        // GET: CECBEmployeeVersion/Details
+        
+        // GET: Employee/Details
         public ActionResult Details()
         {
-            return View();
+            return View($"Details");
         }
 
         /**
@@ -23,56 +23,50 @@ namespace CECBTIMS.Controllers
          * Find Employees in the CECB ERP Database
          * 
          */
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Find([Bind(Include = "EmployeeId")] Employee empl)
+        public async Task<ActionResult> Find(string method, string q)
         {
+            //            FullName, NIC, EPFNo
 
-            if (ModelState.IsValid)
+            if (string.IsNullOrWhiteSpace(method) || string.IsNullOrWhiteSpace(q))
             {
-                var query =
-                    from emp in db.cmn_EmployeeVersion
-                    join wks in db.cmn_WorkSpace on emp.WorkSpaceId equals wks.WorkSpaceId
-                    join wkst in db.cmn_WorkSpaceType on wks.WorkSpaceTypeId equals wkst.WorkSpaceTypeId
-                    join dsg in db.hrm_Designation on emp.DesignationId equals dsg.DesignationId
-                    join dsgc in db.hrm_DesignationCategory on dsg.DesignationCategoryId equals dsgc.DesignationCategoryId
-                    where emp.EmployeeVersionId == empl.EmployeeId && emp.IsActive
-                    select new
-                    {
-                        employee = emp,
-                        workSpace = wks,
-                        workSpaceType = wkst,
-                        designation = dsg,
-                        designationCategory = dsgc
-                    };
-
-                var data = await query.FirstAsync();
-
-                var employee = new Employee()
-                {
-                    EmployeeId = data.employee.EmployeeId,
-                    EPFNo = data.employee.EPFNo,
-                    Title = (Title)int.Parse(data.employee.Title),
-                    NameWithInitial = data.employee.NameWithInitial,
-                    FullName = data.employee.FullName,
-                    NIC = data.employee.NIC,
-                    WorkSpaceName = data.workSpace.WorkSpaceName,
-                    DesignationName = data.designation.DesignationName,
-                    EmployeeRecruitmentType = (RecruitmentType)int.Parse(data.employee.EmployeeRecruitmentType),
-                    EmpStatus = (EmployeeStatus)data.employee.EmpStatus,
-                    DateOfAppointment = data.employee.DateOfAppointment,
-                    TypeOfContract = data.employee.TypeOfContract,
-                    OfficeEmail = data.employee.OfficeEmail,
-                    MobileNumber = data.employee.MobileNumber,
-                    PrivateEmail = data.employee.PrivateEmail
-                };
-
-                return View($"Details",employee);
+                return new System.Web.Mvc.HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var employees = await
+                (from emp in db.cmn_EmployeeVersion
+                 join wks in db.cmn_WorkSpace on emp.WorkSpaceId equals wks.WorkSpaceId
+                 join wkst in db.cmn_WorkSpaceType on wks.WorkSpaceTypeId equals wkst.WorkSpaceTypeId
+                 join dsg in db.hrm_Designation on emp.DesignationId equals dsg.DesignationId
+                 join dsgc in db.hrm_DesignationCategory on dsg.DesignationCategoryId equals dsgc.DesignationCategoryId
+                 where emp.NIC == q && emp.IsActive
+                 select new Employee()
+                 {
+                     EmployeeId = emp.EmployeeId,
+                     EPFNo = emp.EPFNo,
+                     Title = (Title)int.Parse(emp.Title),
+                     NameWithInitial = emp.NameWithInitial,
+                     FullName = emp.FullName,
+                     NIC = emp.NIC,
+                     WorkSpaceName = wks.WorkSpaceName,
+                     DesignationName = dsg.DesignationName,
+                     EmployeeRecruitmentType = (RecruitmentType)int.Parse(emp.EmployeeRecruitmentType),
+                     EmpStatus = (EmployeeStatus)emp.EmpStatus,
+                     DateOfAppointment = emp.DateOfAppointment,
+                     TypeOfContract = emp.TypeOfContract,
+                     OfficeEmail = emp.OfficeEmail,
+                     MobileNumber = emp.MobileNumber,
+                     PrivateEmail = emp.PrivateEmail
+
+                 }).ToListAsync();
+
+
+            return View($"Details", employees);
+
 
         }
+
+        //            https://www.guru99.com/c-sharp-serialization.html
+
 
 
 
