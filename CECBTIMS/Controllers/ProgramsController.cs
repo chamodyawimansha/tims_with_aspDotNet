ed91abb20f7e6671ea07731c2b8ea6e53a6095ee
chamodya <chamodyawimansha@gmail.com>
2019-07-18 15:37:59 +0530

    program edit method added

--- CECBTIMS/Controllers/ProgramsController.cs
+++ CECBTIMS/Controllers/ProgramsController.cs
@@ -122,7 +122,7 @@ namespace CECBTIMS.Controllers
                 return HttpNotFound();
             }
 
-            ViewBag.ProgramType = (int) program.ProgramType;
+            ViewBag.ProgramType = program.ProgramType;
 
             return View(program);
         }
@@ -130,19 +130,19 @@ namespace CECBTIMS.Controllers
         // POST: Programs/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,ProgramType,StartDate,ApplicationClosingDate,ApplicationClosingTime,Brochure,Venue,EndDate,NotifiedBy,NotifiedOn,ProgramHours,DurationInDays,DurationInMonths,Department,Currency,ProgramFee,RegistrationFee,PerPersonFee,NoShowFee,MemberFee,NonMemberFee,StudentFee,RowVersion")] Program program)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(program).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(program);
-            
-        }
+        //        [HttpPost]
+        //        [ValidateAntiForgeryToken]
+        //        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,ProgramType,StartDate,ApplicationClosingDate,ApplicationClosingTime,Brochure,Venue,EndDate,NotifiedBy,NotifiedOn,ProgramHours,DurationInDays,DurationInMonths,Department,Currency,ProgramFee,RegistrationFee,PerPersonFee,NoShowFee,MemberFee,NonMemberFee,StudentFee,RowVersion")] Program program)
+        //        {
+        //            if (ModelState.IsValid)
+        //            {
+        //                db.Entry(program).State = EntityState.Modified;
+        //                await db.SaveChangesAsync();
+        //                return RedirectToAction("Index");
+        //            }
+        //            return View(program);
+        //            
+        //        }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit(int? id, byte[] rowVersion)
@@ -158,66 +158,57 @@ namespace CECBTIMS.Controllers
 
             if (id == null)
             {
-                return new System.Web.Mvc.HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
             var programToUpdate = await db.Programs.FindAsync(id);
+
             if (programToUpdate == null)
             {
-//                Department deletedDepartment = new Department();
-//                TryUpdateModel(deletedDepartment, fieldsToBind);
-//                ModelState.AddModelError(string.Empty,
-//                    "Unable to save changes. The department was deleted by another user.");
-//                ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "FullName", deletedDepartment.InstructorID);
-//                return View(deletedDepartment);
-                return Content("Program Not Found");
+                Program deletedP = new Program();
+                TryUpdateModel(deletedP, fieldsToBind);
+                ModelState.AddModelError(string.Empty, "Unable to save changes. The Program was deleted by another user.");
+
+                return View(deletedP);
             }
 
-//            if (TryUpdateModel(departmentToUpdate, fieldsToBind))
-//            {
-//                try
-//                {
-//                    db.Entry(departmentToUpdate).OriginalValues["RowVersion"] = rowVersion;
-//                    await db.SaveChangesAsync();
-//
-//                    return RedirectToAction("Index");
-//                }
-//                catch (DbUpdateConcurrencyException ex)
-//                {
-//                    var entry = ex.Entries.Single();
-//                    var clientValues = (Department)entry.Entity;
-//                    var databaseEntry = entry.GetDatabaseValues();
-//                    if (databaseEntry == null)
-//                    {
-//                        ModelState.AddModelError(string.Empty,
-//                            "Unable to save changes. The department was deleted by another user.");
-//                    }
-//                    else
-//                    {
-//                        var databaseValues = (Department)databaseEntry.ToObject();
-//
-//                        if (databaseValues.Name != clientValues.Name)
-//                            ModelState.AddModelError("Name", "Current value: "
-//                                + databaseValues.Name);
-//                        if (databaseValues.Budget != clientValues.Budget)
-//                            ModelState.AddModelError("Budget", "Current value: "
-//                                + String.Format("{0:c}", databaseValues.Budget));
-//                        if (databaseValues.StartDate != clientValues.StartDate)
-//                            ModelState.AddModelError("StartDate", "Current value: "
-//                                + String.Format("{0:d}", databaseValues.StartDate));
-//                        if (databaseValues.InstructorID != clientValues.InstructorID)
-//                            ModelState.AddModelError("InstructorID", "Current value: "
-//                                + db.Instructors.Find(databaseValues.InstructorID).FullName);
-//                        ModelState.AddModelError(string.Empty, "The record you attempted to edit "
-//                            + "was modified by another user after you got the original value. The "
-//                            + "edit operation was canceled and the current values in the database "
-//                            + "have been displayed. If you still want to edit this record, click "
-//                            + "the Save button again. Otherwise click the Back to List hyperlink.");
-//                        departmentToUpdate.RowVersion = databaseValues.RowVersion;
-//                    }
-//                }
-            return Content("Program Found");
+            if (TryUpdateModel(programToUpdate, fieldsToBind))
+            {
+                try
+                {
+                    db.Entry(programToUpdate).OriginalValues["RowVersion"] = rowVersion;
+                    await db.SaveChangesAsync();
 
+                    return RedirectToAction($"Details", new {id = id});
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    var entry = ex.Entries.Single();
+                    var clientValues = (Program)entry.Entity;
+                    var databaseEntry = entry.GetDatabaseValues();
+
+                    if (databaseEntry == null)
+                    {
+                        ModelState.AddModelError(string.Empty,
+                            "Unable to save changes. The department was deleted by another user.");
+                    }
+                    else
+                    {
+                        var databaseValues = (Program)databaseEntry.ToObject();
+                    
+                        ModelState.AddModelError(string.Empty, "The Program you attempted to edit was modified by another user after you got the original values. The edit operation was canceled and the current values in the database have been displayed. If you still want to edit this record, click the Save button again. Otherwise click the Back to List hyperlink.");
+                        programToUpdate.RowVersion = databaseValues.RowVersion;
+                    }
+                }
+                catch (RetryLimitExceededException  error)
+                {
+//                    ModelState.AddModelError(error.Message);
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+
+            }
+
+            return View(programToUpdate);
         }
 
         // GET: Programs/Delete/5
