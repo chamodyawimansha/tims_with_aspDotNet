45914a915ceb7bc5c74b89232ef1bdc5d2fad88b
chamodya <chamodyawimansha@gmail.com>
2019-07-23 13:54:24 +0530

    file types enum created

---
+++ CECBTIMS/Models/Enums/FileTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CECBTIMS.Models.Enums
+{
+    public enum FileTypes
+    {
+        docx = 1,
+        pdf,
+        doc,
+        png,
+        jpeg,
+        JPG,
+    }
+}
