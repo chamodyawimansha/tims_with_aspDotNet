d9021f936fc0f3b402cec00a8fff26cea624c22b
chamodya <chamodyawimansha@gmail.com>
2019-08-04 14:43:56 +0530

    file can reupload need to update in the database

--- CECBTIMS/Controllers/FilesController.cs
+++ CECBTIMS/Controllers/FilesController.cs
@@ -83,7 +83,7 @@ namespace CECBTIMS.Controllers
         public async Task<ActionResult> UploadFile(HttpPostedFileBase file,string title,string details,int? programId)
         {
 
-            if (file == null || file.ContentLength <= 0) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            if (file == null || file.ContentLength <= 0 ) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             // get the file extension
             var fileExtension = Path.GetExtension(file.FileName.ToUpper())?.Replace(".", string.Empty);
 
@@ -156,37 +156,88 @@ namespace CECBTIMS.Controllers
 
 
         // GET: Files/Edit/5
-//        public async Task<ActionResult> Edit(Guid? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            TimsFile file = await db.Files.FindAsync(id);
-//            if (file == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", file.ProgramId);
-//            return View(file);
-//        }
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var file = await db.Files.FindAsync(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.ProgramId = file.Program.Id;
+
+            return View(file);
+        }
 
         // POST: Files/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,Details,FileName,OriginalFileName,FileType,FileMethod,ProgramId,CreatedAt,UpdatedAt,CreatedBy,UpdatedBy,RowVersion")] TimsFile file)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                db.Entry(file).State = EntityState.Modified;
-//                await db.SaveChangesAsync();
-//                return RedirectToAction("Index");
-//            }
-//            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", file.ProgramId);
-//            return View(file);
-//        }
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditFile(Guid? Id, HttpPostedFileBase file, int? programId, string title, string details, byte[] rowVersion)
+        {
+            // get the file object from the database
+            var fileInDb = await db.Files.FindAsync(Id);
+
+            if (fileInDb == null) return HttpNotFound();
+
+            var newFileName = "";
+
+            //check if the file is going to change
+            if (file != null && !(file.ContentLength <= 0))
+            {
+                //get the new file ext
+                var newFileExtension = Path.GetExtension(file.FileName.ToUpper())?.Replace(".", string.Empty);
+                //check the new file ext is right
+                if (!Enum.GetNames(typeof(FileType)).Contains(newFileExtension))
+                {
+                    ViewBag.Message = "File type is not supported";
+                    return View($"Edit");
+                }
+
+                var currentFilepath = Path.Combine(Server.MapPath("~/Storage"), Path.GetFileName(fileInDb.FileName) ?? throw new InvalidOperationException());
+
+                //remove the file if available in the storage
+                if (System.IO.File.Exists(currentFilepath))
+                {
+                    System.IO.File.Delete(currentFilepath);
+                }
+
+                //new name
+                newFileName = Id + "_" + file.FileName;
+
+                var newFilePath = Path.Combine(Server.MapPath("~/Storage"), Path.GetFileName(newFileName) ?? throw new InvalidOperationException());
+                // store file in the storage
+                try
+                {
+                    file.SaveAs(newFilePath);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "File upload Failed:" + ex.Message.ToString();
+                    return View($"Edit");
+                }
+         
+
+
+            }
+
+            // update details in the database
+            
+
+
+
+            // if file upload failed remove the data from the storage and notify to re upload
+
+            return Content(programId.ToString());
+
+
+        }
 //
 //        // GET: Files/Delete/5
 //        public async Task<ActionResult> Delete(Guid? id)
