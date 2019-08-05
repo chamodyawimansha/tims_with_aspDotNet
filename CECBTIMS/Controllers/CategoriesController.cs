bab286c52ca89b3adc3f63c0b04325deeab649d2
chamodya <chamodyawimansha@gmail.com>
2019-08-05 18:16:01 +0530

    can remove EmpCategory

--- CECBTIMS/Controllers/CategoriesController.cs
+++ CECBTIMS/Controllers/CategoriesController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using CECBTIMS.DAL;
 using CECBTIMS.Models;
@@ -23,14 +17,10 @@ namespace CECBTIMS.Controllers
             if(programId == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
             ViewBag.ProgramId = programId;
-
-
+            
             return View();
         }
 
-        // POST: Categories/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "Id,ProgramId,EmpCategory")] EmploymentCategory employmentCategory)
@@ -44,63 +34,18 @@ namespace CECBTIMS.Controllers
 
         }
 
-        // GET: Categories/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EmploymentCategory employmentCategory = await db.EmploymentCategories.FindAsync(id);
-            if (employmentCategory == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", employmentCategory.ProgramId);
-            return View(employmentCategory);
-        }
-
-        // POST: Categories/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,ProgramId,CreatedAt,UpdatedAt,CreatedBy,UpdatedBy,RowVersion")] EmploymentCategory employmentCategory)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(employmentCategory).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", employmentCategory.ProgramId);
-            return View(employmentCategory);
-        }
-
-        // GET: Categories/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EmploymentCategory employmentCategory = await db.EmploymentCategories.FindAsync(id);
-            if (employmentCategory == null)
-            {
-                return HttpNotFound();
-            }
-            return View(employmentCategory);
-        }
-
         // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
+        public async Task<ActionResult> DeleteConfirmed(int id, int programId)
         {
-            EmploymentCategory employmentCategory = await db.EmploymentCategories.FindAsync(id);
+            
+            var employmentCategory = await db.EmploymentCategories.FindAsync(id);
             db.EmploymentCategories.Remove(employmentCategory);
+
             await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+
+            return RedirectToAction("Details", $"Programs", new {id = programId });
         }
 
         protected override void Dispose(bool disposing)
