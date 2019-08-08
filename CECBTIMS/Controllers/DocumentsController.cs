79af8270bbf43190bff9543a334df5a554fd19fb
chamodya <chamodyawimansha@gmail.com>
2019-08-08 17:47:21 +0530

    documents controller

--- CECBTIMS/Controllers/DocumentsController.cs
+++ CECBTIMS/Controllers/DocumentsController.cs
@@ -16,93 +16,119 @@ using DocumentFormat.OpenXml.Drawing;
 
 
 namespace CECBTIMS.Controllers
-{
+{   
     public class DocumentsController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
 
-        // GET: Document
-        public ActionResult Index()
+        private readonly string[] _varList =
         {
-            return Content("Hello");
+            "YEAR",
+            "TODAY",
+            "PROGRAMTITLE",
+            "ORGANISEDBY",
+            "STARTDATE",
+            "VENUE",
+            "MEMBERFEE",
+            "NONMEMBERFEE",
+            "STUDENTFEE",
+        };
+
+        public async Task<ActionResult> Generate(Guid? id, int programId, int? employeeId, bool download)
+        {
+            /**
+             * Validate request
+             */
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            if (await _db.Programs.FindAsync(programId) == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            /**
+             * select the file record in the database. return bad request if not available
+             */
+            var file = await _db.Files.FindAsync(id);
+            if(file == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            /**
+             * get the document template from the storage
+             */
+            var template = System.IO.Path.Combine(Server.MapPath("~/Storage"), System.IO.Path.GetFileName(file.FileName) ?? throw new InvalidOperationException());
+            /**
+             * Generate a new file name and select a path for the new document
+             */
+            var destinationFileName = Guid.NewGuid().ToString("N") + ".docx";
+            var destinationFile = Server.MapPath("~/Storage/gen/"+ destinationFileName);
+            /**
+             * Copy the template with the new name for processing.
+             */
+            System.IO.File.Copy(template, destinationFile, false);
+            /**
+             * Process the document
+             */
+             Process(destinationFile, destinationFileName, programId, employeeId);
+            /**
+             * if download true download the file and return to the page, otherwise return
+             */
+            if (download == true) return Download(destinationFile, destinationFileName);
+
+
+            //return to page
+            return Content("page returned");
         }
 
-
-        public async Task<ActionResult> Generate(Guid? id, int programId)
+        // process the document generation
+        private void Process(string destinationFile,string destinationFileName, int programId, int? employeeId)
         {
-
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-            var file = await db.Files.FindAsync(id);
-
-            if(file == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-            if(await db.Programs.FindAsync(programId) == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-            var template = System.IO.Path.Combine(Server.MapPath("~/Storage"), System.IO.Path.GetFileName(file.FileName) ?? throw new InvalidOperationException());
-            
-            //get date time in mili seconds and generate file name
-            var destinationFileName = Guid.NewGuid().ToString("N") + ".docx";
-
-            var destinationFile = Server.MapPath("~/Storage/gen/"+ destinationFileName);
-
-            System.IO.File.Copy(template, destinationFile, false);
-
-            //integer objects
-            string[] VarList =
-            {
-                "YEAR",
-                "TODAY",
-                "PROGRAMTITLE",
-                "ORGANISEDBY",
-                "STARTDATE",
-                "VENUE",
-                "MEMBERFEE",
-                "NONMEMBERFEE",
-                "STUDENTFEE",
-            };
-
-            //// filepath is a string which contains the path where the new document has to be created
-            
             using (var wordDoc = WordprocessingDocument.Open(destinationFile, true))
             {
                 string docText = null;
+                /**
+                 * store the document in a string variable
+                 */
                 using (var sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
                 {
                     docText = sr.ReadToEnd();
                 }
-
-
-
+                /**
+                 * Instantiate the document helper class with parameters
+                 */
                 var helperClass = typeof(DocumentHelper);
-
                 var classInstance = Activator.CreateInstance(helperClass, programId);
-
-
-                // replace variables in the document
-                foreach (var var in VarList)
+                if (employeeId != null) classInstance = Activator.CreateInstance(helperClass, programId, employeeId);
+                /**
+                 * Replacing the values in the template
+                 */
+                foreach (var var in _varList)
                 {
-                    if (docText.Contains("VAR" + var))
-                    {
-                        var method = helperClass.GetMethod(Helpers.FigureVarName(var));
+                    if (!docText.Contains("VAR" + var)) continue;
+                    var method = helperClass.GetMethod(Helpers.FigureVarName(var));
 
-                        docText = method != null ? new Regex("VAR" + var).Replace(docText, (string)method.Invoke(classInstance, null)) : new Regex("VAR" + var).Replace(docText, "Null");
-                    }
+                    docText = method != null ? new Regex("VAR" + var).Replace(docText, (string)method.Invoke(classInstance, null)) : new Regex("VAR" + var).Replace(docText, "Null");
                 }
 
+                
+                /**
+                 * Process table values
+                 */
+
+
+                /**
+                 * write the changes to the file
+                 */
                 using (var sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
                 {
                     sw.Write(docText);
                 }
+                /**
+                 * Save the document in the database
+                 */
+                Save();
             }
-
-            return DownloadDoc(destinationFile, destinationFileName);
-
-
+        }
+        // save generated file in th database
+        private bool Save()
+        {
+            return false;
         }
 
-
-        public FileResult DownloadDoc(string path, string fileName)
+        private FileResult Download(string path, string fileName)
         {
             return File(path, MimeMapping.GetMimeMapping(path), fileName);
         }
