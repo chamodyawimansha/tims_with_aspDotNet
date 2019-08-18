44561e8574765a79f7e0b0d1e3935fea6915297d
chamodya <chamodyawimansha@gmail.com>
2019-08-18 07:08:56 +0530

    async all

--- CECBTIMS/Controllers/DocumentsController.cs
+++ CECBTIMS/Controllers/DocumentsController.cs
@@ -1,8 +1,15 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using CECBTIMS.DAL;
+using CECBTIMS.Models;
 using CECBTIMS.Models.Document;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Ajax.Utilities;
 
 
 namespace CECBTIMS.Controllers
@@ -10,42 +17,99 @@ namespace CECBTIMS.Controllers
     public class DocumentsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+        private Type _helperClass;
+        private object _classInstance;
 
-        // variable names in the document will be like this get_today, get_program_title, get_program_end_date, get_program_start_date
-
-
-        /*
-         * Show list of documents each program have
-         */
-
-        /**
-         * show document generation form
-         */
-
-        /**
-         *
-         */
-
-
-        /**
-         * static document gen
-         */
-
-        public async Task<ActionResult> Generate()
+        private void InstantiateHelperClass()
         {
-//            var lc = new LocalApprovalLetter(await db.Programs.FindAsync(1));
-            var program = await db.Programs.FindAsync(1);
 
-            var tc = new EmployeesController();
-
-            if (program == null) return Content("Hello");
+        }
 
 
-            var lc = new LocalLongApprovalLetterPOne(program, (tc.GetTrainees(program.Id)).First());
-            lc.Create();
+        public async Task<ActionResult> Process(string option, int programId, Guid? employeeId)
+        {
+            if (option.Equals("ProgramDocument"))
+            {
+                await ProcessProgramDocument(programId);
+
+            }else if (option.Equals("EmployeeDocument"))
+            {
+                return Content("Trainee");
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        private async Task<ActionResult> ProcessProgramDocument(int? programId)
+        {
+            var path = ProcessTemplate();
+
+            IDictionary<string, BookmarkStart> bookmarkMap = new Dictionary<string, BookmarkStart>();
+
+            using (var wordDoc = WordprocessingDocument.Open(path, true))
+            {   
+                // get all the bookmarks from the document
+                foreach (var bookmarkStart in wordDoc.MainDocumentPart.RootElement.Descendants<BookmarkStart>())
+                {
+                    bookmarkMap[bookmarkStart.Name] = bookmarkStart;
+                }
+                // loop the bookmarks
+                foreach (var bookmarkStart in bookmarkMap.Values)
+                {
+
+
+                    // process variable bookmarks 
+
+                    // process list bookmarks
+
+                    //process table bookmarks
+
+//
+//                    YearVar
+//                        TraineeTable
+//                            ProgramList
+
+
+                    var bookmarkText = bookmarkStart.NextSibling<Run>();
+
+                    if (bookmarkText != null)
+                    {
+                        bookmarkText.GetFirstChild<Text>().Text = bookmarkText.InnerText;
+                    }
+                }
+
+            }
 
             return Content("Hello");
         }
+
+        private string ProcessTemplate()
+        {
+
+            var template = Server.MapPath("~/Storage/templates/ApprovelLetter.docx");
+            var tPath = Server.MapPath("~/Storage/gen/"+new Guid()+".docx");
+            System.IO.File.Copy(template, tPath, true);
+
+            return tPath;
+
+
+            //            /**
+            //             * get the document template from the storage
+            //             */
+            //            var template = System.IO.Path.Combine(Server.MapPath("~/Storage"),
+            //                System.IO.Path.GetFileName(file.FileName) ?? throw new InvalidOperationException());
+            //            /**
+            //             * Generate a new file name and select a path for the new document
+            //             */
+            //            this._destinationFileName = Guid.NewGuid().ToString("N") + ".docx";
+            //            this._destinationFile = Server.MapPath("~/Storage/gen/" + _destinationFileName);
+            //            /**
+            //             * Copy the template with the new name for processing.
+            //             */
+            //            System.IO.File.Copy(template, _destinationFile, false);
+        }
+
+
 
     }
 }
--- CECBTIMS/Models/DocumentHelper.cs
+++ CECBTIMS/Models/DocumentHelper.cs
@@ -8,89 +8,141 @@ namespace CECBTIMS.Models
 {
     public class DocumentHelper
     {
-        // get organisers of the program
-        public static string GetOrganisedBy(Program program)
+        // training program
+        private Program program;
+        //list of trainees
+        private List<Employee> employees;
+        private Employee employee;
+
+        public DocumentHelper(int programId)
         {
-            return program.ProgramArrangements.Aggregate("", (current, item) => current == "" ? item.Organizer.Name : current + ", " + item.Organizer.Name);
+            var ec = new EmployeesController();
+
+            this.program = ProgramsController.FindProgram(programId);
+            this.employees = ec.GetTrainees(programId);
         }
 
-        public static string GetTargetGroup(Program program)
+        public DocumentHelper(int programId, Guid EmployeeId)
         {
-            return program.TargetGroups.Aggregate("", (current, item) => current == "" ? item.Name : current + ", " + item.Name);
+
         }
 
-        public static string GetTargetGroupList(Program program)
-        {
-            return program.TargetGroups.Aggregate("", (current, item) => current == "" ? item.Name : current + ", " + item.Name);
-        }
 
-        public static string GetTraineeManagerName()
-        {
-            return "Eng.L C K Karunaratne";
-        }
 
-        public static string ToFunctionName(string name)
-        {
-            name = name.ToLower();
-            var nameParts = name.Split(null);
-            return nameParts.Aggregate("Get", (current, item) => current + (item.First().ToString().ToUpper() + item.Substring(1)));
-        }
 
-        private static string ToUpperFirstLetter(string source)
-        {
-            if (string.IsNullOrEmpty(source))
-                return string.Empty;
-            var letters = source.ToCharArray();
-            letters[0] = char.ToUpper(letters[0]);
 
-            return new string(letters);
-        }
 
-        private static string ToProperName(string name)
-        {
-            var nameParts = name.Split(null);
-            var newName = "";
 
-            for (var i = 1; i < nameParts.Length; i++)
-            {
-                if (!nameParts[i].Equals("."))
-                {
-                    newName += nameParts[i] + $" ";
-                }
-            }
 
-            return newName += ToUpperFirstLetter(nameParts[0].ToLower());
-        }
 
-        public static string GetName(Employee trainee)
-        {
-            return trainee.Title != null  ? trainee.Title+". "+ToProperName(trainee.NameWithInitial) : ToProperName(trainee.NameWithInitial);
-        }
 
-        public static string GetDesignation(Employee trainee)
-        {
-            return trainee.DesignationName;
-        }
 
-        public static string GetNatureOfAppointment(Employee trainee)
-        {
-            return trainee.EmployeeRecruitmentType.ToString();
-        }
 
-        public static string GetRecommendation(Employee trainee)
-        {
-            return trainee.WorkSpaceType;
-        }
 
-        public static string GetDurationOfTheCourse(Program program)
-        {
-            return program.DurationInMonths.ToString();
-        }
 
-        public static string GetCourseFee(Program program)
-        {
-            return program.ProgramFee.ToString();
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        // get organisers of the program
+//        public static string GetOrganisedBy(Program program)
+//        {
+//            return program.ProgramArrangements.Aggregate("", (current, item) => current == "" ? item.Organizer.Name : current + ", " + item.Organizer.Name);
+//        }
+//
+//        public static string GetTargetGroup(Program program)
+//        {
+//            return program.TargetGroups.Aggregate("", (current, item) => current == "" ? item.Name : current + ", " + item.Name);
+//        }
+//
+//        public static string GetTargetGroupList(Program program)
+//        {
+//            return program.TargetGroups.Aggregate("", (current, item) => current == "" ? item.Name : current + ", " + item.Name);
+//        }
+//
+//        public static string GetTraineeManagerName()
+//        {
+//            return "Eng.L C K Karunaratne";
+//        }
+//
+//        public static string ToFunctionName(string name)
+//        {
+//            name = name.ToLower();
+//            var nameParts = name.Split(null);
+//            return nameParts.Aggregate("Get", (current, item) => current + (item.First().ToString().ToUpper() + item.Substring(1)));
+//        }
+//
+//        private static string ToUpperFirstLetter(string source)
+//        {
+//            if (string.IsNullOrEmpty(source))
+//                return string.Empty;
+//            var letters = source.ToCharArray();
+//            letters[0] = char.ToUpper(letters[0]);
+//
+//            return new string(letters);
+//        }
+//
+//        private static string ToProperName(string name)
+//        {
+//            var nameParts = name.Split(null);
+//            var newName = "";
+//
+//            for (var i = 1; i < nameParts.Length; i++)
+//            {
+//                if (!nameParts[i].Equals("."))
+//                {
+//                    newName += nameParts[i] + $" ";
+//                }
+//            }
+//
+//            return newName += ToUpperFirstLetter(nameParts[0].ToLower());
+//        }
+//
+//        public static string GetName(Employee trainee)
+//        {
+//            return trainee.Title != null  ? trainee.Title+". "+ToProperName(trainee.NameWithInitial) : ToProperName(trainee.NameWithInitial);
+//        }
+//
+//        public static string GetDesignation(Employee trainee)
+//        {
+//            return trainee.DesignationName;
+//        }
+//
+//        public static string GetNatureOfAppointment(Employee trainee)
+//        {
+//            return trainee.EmployeeRecruitmentType.ToString();
+//        }
+//
+//        public static string GetRecommendation(Employee trainee)
+//        {
+//            return trainee.WorkSpaceType;
+//        }
+//
+//        public static string GetDurationOfTheCourse(Program program)
+//        {
+//            return program.DurationInMonths.ToString();
+//        }
+//
+//        public static string GetCourseFee(Program program)
+//        {
+//            return program.ProgramFee.ToString();
+//        }
 
 
     }
