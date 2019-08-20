c89359b3a66d8661c5643ca10f00b1170fa2162f
chamodya <chamodyawimansha@gmail.com>
2019-08-20 21:54:34 +0530

    table col enum created and added to the default column create

--- CECBTIMS/Models/DefaultColumn.cs
+++ CECBTIMS/Models/DefaultColumn.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CECBTIMS.Models.Enums;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace CECBTIMS.Models
 {
     public class DefaultColumn : BaseCols
     {
         public int Id { get; set; }
-        public string ColumnName { get; set; }
+        public TableColumnName ColumnName { get; set; }
         public int TemplateId { get; set; }
 
         public Template Template { get; set; }
