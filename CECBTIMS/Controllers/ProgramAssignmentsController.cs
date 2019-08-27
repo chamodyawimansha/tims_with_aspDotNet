327b13b6024c8a249415c8c160ef30609391335c
chamodya <chamodyawimansha@gmail.com>
2019-08-27 15:27:16 +0530

    employee select form created

--- CECBTIMS/Controllers/ProgramAssignmentsController.cs
+++ CECBTIMS/Controllers/ProgramAssignmentsController.cs
@@ -17,35 +17,11 @@ namespace CECBTIMS.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: ProgramAssignments
-//        public async Task<ActionResult> Index()
-//        {
-//            var programAssignments = db.ProgramAssignments.Include(p => p.Program);
-//            return View(await programAssignments.ToListAsync());
-//        }
 
-        // GET: ProgramAssignments/Details/5
-//        public async Task<ActionResult> Details(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            ProgramAssignment programAssignment = await db.ProgramAssignments.FindAsync(id);
-//            if (programAssignment == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(programAssignment);
-//        }
+        public ActionResult Select()
+        {
 
-        // GET: ProgramAssignments/Create
-//        public ActionResult Create()
-//        {
-//            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title");
-//            return View();
-//        }
-
+        }
         // POST: ProgramAssignments/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
@@ -62,54 +38,6 @@ namespace CECBTIMS.Controllers
             }
 
             return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        }
-
-        // GET: ProgramAssignments/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ProgramAssignment programAssignment = await db.ProgramAssignments.FindAsync(id);
-            if (programAssignment == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", programAssignment.ProgramId);
-            return View(programAssignment);
-        }
-
-        // POST: ProgramAssignments/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,EmployeeId,EmployeeVersionId,EPFNo,ProgramId,CreatedAt,UpdatedAt,CreatedBy,UpdatedBy,RowVersion")] ProgramAssignment programAssignment)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(programAssignment).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", programAssignment.ProgramId);
-            return View(programAssignment);
-        }
-
-        // GET: ProgramAssignments/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ProgramAssignment programAssignment = await db.ProgramAssignments.FindAsync(id);
-            if (programAssignment == null)
-            {
-                return HttpNotFound();
-            }
-            return View(programAssignment);
         }
 
         // POST: ProgramAssignments/Delete/5
