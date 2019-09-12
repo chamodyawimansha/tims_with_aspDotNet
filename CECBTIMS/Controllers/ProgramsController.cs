196cb3b03c57f8bcb7e18b16a625715ffc7f9bfd
chamodya wimansha <chamodyawimansha@gmail.com>
2019-09-12 20:37:35 +0530

    program find form done

--- CECBTIMS/Controllers/ProgramsController.cs
+++ CECBTIMS/Controllers/ProgramsController.cs
@@ -11,6 +11,7 @@ using System.Web;
 using System.Web.Mvc;
 using CECBTIMS.DAL;
 using CECBTIMS.Models;
+using CECBTIMS.Models.Enums;
 using PagedList.EntityFramework;
 
 namespace CECBTIMS.Controllers
@@ -22,9 +23,9 @@ namespace CECBTIMS.Controllers
         private static ApplicationDbContext dbs = new ApplicationDbContext();
 
         // GET: Programs
-        public async Task<ActionResult> Index(string sortOrder, int? countPerPage, string currentFilter, string searchString, int? page)
+        public async Task<ActionResult> Index(string sortOrder, int? countPerPage, string currentFilter,
+            string searchString, int? page)
         {
-            
             ViewBag.CurrentSort = sortOrder;
             ViewBag.serachParam = searchString;
             ViewBag.TitleSortParm = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
@@ -44,7 +45,7 @@ namespace CECBTIMS.Controllers
             ViewBag.CurrentFilter = searchString;
 
             var programs = from s in db.Programs
-                           select s;
+                select s;
 
             if (!string.IsNullOrEmpty(searchString))
             {
@@ -74,7 +75,7 @@ namespace CECBTIMS.Controllers
                 case "createddate_desc":
                     programs = programs.OrderByDescending(s => s.CreatedAt);
                     break;
-                default:  
+                default:
                     programs = programs.OrderBy(s => s.Title);
                     break;
             }
@@ -84,21 +85,61 @@ namespace CECBTIMS.Controllers
             ViewBag.PageNumber = pageNumber;
 
 
-
             return View(await programs.ToPagedListAsync(pageNumber, pageSize));
-
-
         }
 
         public async Task<ActionResult> Find()
         {
             return View();
         }
+
         [HttpPost, ActionName("Find")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> FindPost()
+        public async Task<ActionResult> FindPost(string searchMethod, string programType, string searchString)
         {
-            return View();
+            var programs = from s in db.Programs
+                select s;
+
+            switch (searchMethod)
+            {
+                case "Title":
+                    programs = programs.Where(p => p.Title.Contains(searchString));
+                    break;
+                case "Organiser":
+                    programs = programs.Where(p => p.Organizer.Name.Contains(searchString));
+                    break;
+                case "ApplicationClosingDate":
+                    var closingDate = DateTime.Parse(searchString);
+                    programs = programs.Where(p => p.ApplicationClosingDate == closingDate);
+                    break;
+                case "StartDate":
+                    var startDate = DateTime.Parse(searchString);
+                    programs = programs.Where(p => p.StartDate == startDate);
+                    break;
+                default:
+                    var createdDate = DateTime.Parse(searchString);
+                    programs = programs.Where(p => p.CreatedAt == createdDate);
+                    break;
+            }
+
+            switch (programType)
+            {
+                case "LocalProgram":
+                    programs = programs.Where(p => p.ProgramType.ToString().Contains("Local"));
+                    break;
+                case "ForeignProgram":
+                    programs = programs.Where(p => p.ProgramType.ToString().Contains("Foreign"));
+                    break;
+                case "PostGradProgram":
+                    programs = programs.Where(p => p.ProgramType.ToString().Contains("PostGraduation"));
+                    break;
+                case "InHouseProgram":
+                    programs = programs.Where(p => p.ProgramType.ToString().Contains("InHouse"));
+                    break;
+            }
+
+
+            return View(await programs.ToListAsync());
         }
 
         // GET: Programs/Details/5
@@ -108,6 +149,7 @@ namespace CECBTIMS.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             var program = await db.Programs.FindAsync(id);
             if (program == null)
             {
@@ -137,7 +179,7 @@ namespace CECBTIMS.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> SelectOrg(int? programId, int? orgId)
         {
-            if(programId == null || orgId == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            if (programId == null || orgId == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             //get the program
             var program = await db.Programs.FindAsync(programId);
             if (program == null) return HttpNotFound();
@@ -147,7 +189,7 @@ namespace CECBTIMS.Controllers
             await db.SaveChangesAsync();
 
 
-            return RedirectToAction($"Details", $"Programs",new{id=program.Id});
+            return RedirectToAction($"Details", $"Programs", new {id = program.Id});
         }
 
         [HttpPost]
@@ -163,36 +205,37 @@ namespace CECBTIMS.Controllers
             program.OrganizerId = null;
             await db.SaveChangesAsync();
 
-            return RedirectToAction($"Details", $"Programs", new { id = program.Id });
+            return RedirectToAction($"Details", $"Programs", new {id = program.Id});
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "Id,Title,ProgramType,StartDate,StartTime,EndTime,ApplicationClosingDate,ApplicationClosingTime,Venue,EndDate,NotifiedBy,NotifiedOn,ProgramHours,DurationInDays,DurationInMonths,Department,Currency,ProgramFee,RegistrationFee,PerPersonFee,NoShowFee,MemberFee,NonMemberFee,StudentFee")] Program program)
+        public async Task<ActionResult> Create([Bind(Include =
+                "Id,Title,ProgramType,StartDate,StartTime,EndTime,ApplicationClosingDate,ApplicationClosingTime,Venue,EndDate,NotifiedBy,NotifiedOn,ProgramHours,DurationInDays,DurationInMonths,Department,Currency,ProgramFee,RegistrationFee,PerPersonFee,NoShowFee,MemberFee,NonMemberFee,StudentFee")]
+            Program program)
         {
             if (!ModelState.IsValid) return View(program);
 
             db.Programs.Add(program);
             await db.SaveChangesAsync();
             return RedirectToAction($"Index");
-
         }
 
         // GET: Programs/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
-
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Program program = await db.Programs.FindAsync(id);
             if (program == null)
             {
                 return HttpNotFound();
             }
 
-            ViewBag.ProgramType = (int)program.ProgramType;
+            ViewBag.ProgramType = (int) program.ProgramType;
 
             return View(program);
         }
@@ -201,10 +244,10 @@ namespace CECBTIMS.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit(int? id, byte[] rowVersion)
         {
-
             string[] fieldsToBind = new string[]
             {
-                "Title", "ProgramType", "StartDate","StartTime","EndTime", "ApplicationClosingDate", "ApplicationClosingTime",
+                "Title", "ProgramType", "StartDate", "StartTime", "EndTime", "ApplicationClosingDate",
+                "ApplicationClosingTime",
                 "Venue", "EndDate", "NotifiedBy", "NotifiedOn", "ProgramHours", "DurationInDays", "DurationInMonths",
                 "Department", "Currency", "ProgramFee", "RegistrationFee", "PerPersonFee", "NoShowFee", "MemberFee",
                 "NonMemberFee", "StudentFee", "RowVersion"
@@ -221,7 +264,8 @@ namespace CECBTIMS.Controllers
             {
                 Program deletedP = new Program();
                 TryUpdateModel(deletedP, fieldsToBind);
-                ModelState.AddModelError(string.Empty, "Unable to save changes. The Program was deleted by another user.");
+                ModelState.AddModelError(string.Empty,
+                    "Unable to save changes. The Program was deleted by another user.");
 
                 return View(deletedP);
             }
@@ -238,7 +282,7 @@ namespace CECBTIMS.Controllers
                 catch (DbUpdateConcurrencyException ex)
                 {
                     var entry = ex.Entries.Single();
-                    var clientValues = (Program)entry.Entity;
+                    var clientValues = (Program) entry.Entity;
                     var databaseEntry = entry.GetDatabaseValues();
 
                     if (databaseEntry == null)
@@ -248,109 +292,134 @@ namespace CECBTIMS.Controllers
                     }
                     else
                     {
-                        var databaseValues = (Program)databaseEntry.ToObject();
+                        var databaseValues = (Program) databaseEntry.ToObject();
 
                         if (databaseValues.Title != clientValues.Title)
                         {
                             ModelState.AddModelError("Title", "Current value: " + databaseValues.Title);
                         }
+
                         if (databaseValues.StartDate != clientValues.StartDate)
                         {
                             ModelState.AddModelError("StartDate", "Current value: " + databaseValues.StartDate);
                         }
+
                         if (databaseValues.ApplicationClosingDate != clientValues.ApplicationClosingDate)
                         {
-                            ModelState.AddModelError("ApplicationClosingDate", "Current value: " + databaseValues.ApplicationClosingDate);
+                            ModelState.AddModelError("ApplicationClosingDate",
+                                "Current value: " + databaseValues.ApplicationClosingDate);
                         }
+
                         if (databaseValues.ApplicationClosingTime != clientValues.ApplicationClosingTime)
                         {
-                            ModelState.AddModelError("ApplicationClosingTime", "Current value: " + databaseValues.ApplicationClosingTime);
+                            ModelState.AddModelError("ApplicationClosingTime",
+                                "Current value: " + databaseValues.ApplicationClosingTime);
                         }
+
                         if (databaseValues.Venue != clientValues.Venue)
                         {
                             ModelState.AddModelError("Venue", "Current value: " + databaseValues.Venue);
                         }
+
                         if (databaseValues.EndDate != clientValues.EndDate)
                         {
                             ModelState.AddModelError("EndDate", "Current value: " + databaseValues.EndDate);
                         }
+
                         if (databaseValues.NotifiedBy != clientValues.NotifiedBy)
                         {
                             ModelState.AddModelError("NotifiedBy", "Current value: " + databaseValues.NotifiedBy);
                         }
+
                         if (databaseValues.NotifiedOn != clientValues.NotifiedOn)
                         {
                             ModelState.AddModelError("NotifiedOn", "Current value: " + databaseValues.NotifiedOn);
                         }
+
                         if (databaseValues.ProgramHours != clientValues.ProgramHours)
                         {
                             ModelState.AddModelError("ProgramHours", "Current value: " + databaseValues.ProgramHours);
                         }
+
                         if (databaseValues.DurationInDays != clientValues.DurationInDays)
                         {
-                            ModelState.AddModelError("DurationInDays", "Current value: " + databaseValues.DurationInDays);
+                            ModelState.AddModelError("DurationInDays",
+                                "Current value: " + databaseValues.DurationInDays);
                         }
+
                         if (databaseValues.DurationInMonths != clientValues.DurationInMonths)
                         {
-                            ModelState.AddModelError("DurationInMonths", "Current value: " + databaseValues.DurationInMonths);
+                            ModelState.AddModelError("DurationInMonths",
+                                "Current value: " + databaseValues.DurationInMonths);
                         }
+
                         if (databaseValues.Department != clientValues.Department)
                         {
                             ModelState.AddModelError("Department", "Current value: " + databaseValues.Department);
                         }
+
                         if (databaseValues.Currency != clientValues.Currency)
                         {
                             ModelState.AddModelError("Currency", "Current value: " + databaseValues.Currency);
                         }
+
                         if (!databaseValues.ProgramFee.Equals(clientValues.ProgramFee))
                         {
                             ModelState.AddModelError("ProgramFee", "Current value: " + databaseValues.ProgramFee);
                         }
+
                         if (!databaseValues.RegistrationFee.Equals(clientValues.RegistrationFee))
                         {
-                            ModelState.AddModelError("RegistrationFee", "Current value: " + databaseValues.RegistrationFee);
+                            ModelState.AddModelError("RegistrationFee",
+                                "Current value: " + databaseValues.RegistrationFee);
                         }
+
                         if (!databaseValues.PerPersonFee.Equals(clientValues.PerPersonFee))
                         {
                             ModelState.AddModelError("PerPersonFee", "Current value: " + databaseValues.PerPersonFee);
                         }
+
                         if (!databaseValues.NoShowFee.Equals(clientValues.NoShowFee))
                         {
                             ModelState.AddModelError("NoShowFee", "Current value: " + databaseValues.NoShowFee);
                         }
+
                         if (!databaseValues.MemberFee.Equals(clientValues.MemberFee))
                         {
                             ModelState.AddModelError("MemberFee", "Current value: " + databaseValues.MemberFee);
                         }
+
                         if (!databaseValues.NonMemberFee.Equals(clientValues.NonMemberFee))
                         {
                             ModelState.AddModelError("NonMemberFee", "Current value: " + databaseValues.NonMemberFee);
                         }
+
                         if (!databaseValues.StudentFee.Equals(clientValues.StudentFee))
                         {
                             ModelState.AddModelError("StudentFee", "Current value: " + databaseValues.StudentFee);
                         }
 
 
-                        ModelState.AddModelError(string.Empty, "The Program you attempted to edit was modified by another user after you got the original values. The edit operation was canceled and the current values in the database have been displayed. If you still want to edit this record, click the Save button again. Otherwise click the Back Link.");
+                        ModelState.AddModelError(string.Empty,
+                            "The Program you attempted to edit was modified by another user after you got the original values. The edit operation was canceled and the current values in the database have been displayed. If you still want to edit this record, click the Save button again. Otherwise click the Back Link.");
                         programToUpdate.RowVersion = databaseValues.RowVersion;
                     }
                 }
-                catch (RetryLimitExceededException  /*error*/)
+                catch (RetryLimitExceededException /*error*/)
                 {
 //                    ModelState.AddModelError(error.Message);
-                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                    ModelState.AddModelError("",
+                        "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
                 }
-
             }
 
-            ViewBag.ProgramType = (int)programToUpdate.ProgramType;
+            ViewBag.ProgramType = (int) programToUpdate.ProgramType;
             return View(programToUpdate);
         }
 
         internal static async Task<Program> GetProgram(int id)
         {
-            var program =  await dbs.Programs.FindAsync(id);
+            var program = await dbs.Programs.FindAsync(id);
 
             return program ?? new Program();
         }
@@ -362,11 +431,13 @@ namespace CECBTIMS.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Program program = await db.Programs.FindAsync(id);
             if (program == null)
             {
                 return HttpNotFound();
             }
+
             return View(program);
         }
 
@@ -375,7 +446,6 @@ namespace CECBTIMS.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-
             Program program = await db.Programs.FindAsync(id);
             if (program == null)
             {
@@ -396,6 +466,7 @@ namespace CECBTIMS.Controllers
             {
                 db.Dispose();
             }
+
             base.Dispose(disposing);
         }
     }
