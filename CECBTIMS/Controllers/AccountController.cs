7855e19b231a2bc964b8a49058aa0b69bb06878a
chamodya wimansha <chamodyawimansha@gmail.com>
2019-09-14 11:48:12 +0530

    can login with default admin account

--- CECBTIMS/Controllers/AccountController.cs
+++ CECBTIMS/Controllers/AccountController.cs
@@ -164,35 +164,6 @@ namespace CECBTIMS.Controllers
             return View();
         }
 
-        //create roles and admin account for the first time
-        protected internal static void createRolesAndUsers()
-        {
-            var context = new ApplicationDbContext();
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-            // In Startup iam creating first Admin Role and creating a default Admin User     
-            if (!roleManager.RoleExists("Administrator"))
-            {
-                // Admin Role    
-                var role = new IdentityRole { Name = "Administrator" };
-                roleManager.Create(role);
-
-                var user = new ApplicationUser { UserName = "Administrator", Email = "Admin@timsadmin.com" };
-                
-                var chkUser = userManager.Create(user, "password");
-                //Add default User to Role Admin    
-                if (chkUser.Succeeded)
-                {
-                    userManager.AddToRole(user.Id, "Administrator");
-                }
-            }
-            // creating Creating Employee role     
-            if (roleManager.RoleExists("User")) return;
-            {
-                var role = new IdentityRole { Name = "User" };
-                roleManager.Create(role);
-            }
-        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- CECBTIMS/Startup.cs
+++ CECBTIMS/Startup.cs
@@ -15,7 +15,37 @@ namespace CECBTIMS
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            AccountController.createRolesAndUsers();
+            createRolesAndUsers();
+        }
+
+        //create roles and admin account for the first time
+        private static void createRolesAndUsers()
+        {
+            var context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            // In Startup iam creating first Admin Role and creating a default Admin User     
+            if (!roleManager.RoleExists("Administrator"))
+            {
+                // Admin Role    
+                var role = new IdentityRole { Name = "Administrator" };
+                roleManager.Create(role);
+
+                var user = new ApplicationUser { UserName = "admin", Email = "admin@timsadmin.com" };
+
+                var chkUser = userManager.Create(user, "password");
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Administrator");
+                }
+            }
+            // creating Creating Employee role     
+            if (roleManager.RoleExists("User")) return;
+            {
+                var role = new IdentityRole { Name = "User" };
+                roleManager.Create(role);
+            }
         }
     }
 }
