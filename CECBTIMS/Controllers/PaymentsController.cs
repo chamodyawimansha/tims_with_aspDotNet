1d90ecde9e6e1ef96acc9c60ab022f1bdf372353
chamodya wimansha <chamodyawimansha@gmail.com>
2019-09-18 13:04:16 +0530

    payments controller and views created

---
+++ CECBTIMS/Controllers/PaymentsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CECBTIMS.DAL;
+using CECBTIMS.Models;
+
+namespace CECBTIMS.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Payments
+        public async Task<ActionResult> Index()
+        {
+            var payments = db.Payments.Include(p => p.Program);
+            return View(await payments.ToListAsync());
+        }
+
+        // GET: Payments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = await db.Payments.FindAsync(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title");
+            return View();
+        }
+
+        // POST: Payments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,ProgramId,WorkSpaceId,Title,value,ChequeNo,ChequeFile,CreatedAt,UpdatedAt,ApplicationUserId,UpdatedBy,RowVersion")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Payments.Add(payment);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", payment.ProgramId);
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = await db.Payments.FindAsync(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", payment.ProgramId);
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,ProgramId,WorkSpaceId,Title,value,ChequeNo,ChequeFile,CreatedAt,UpdatedAt,ApplicationUserId,UpdatedBy,RowVersion")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payment).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProgramId = new SelectList(db.Programs, "Id", "Title", payment.ProgramId);
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = await db.Payments.FindAsync(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Payment payment = await db.Payments.FindAsync(id);
+            db.Payments.Remove(payment);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
